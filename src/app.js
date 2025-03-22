0ccd0beab507b775b6dffa4ef96522330741a181
Dimitris Stylianou <dimitrisstylianou@proton.me>
2025-03-22 23:12:56 +0200

    Update app.js
    - Add middleware route support with the base URL `/api/country`.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import morgan from "morgan";
 import express from "express";
+import countryRouter from "./routers/country.router.js";
 
 const app = express();
+
 app.use(express.json());
 app.use(morgan("dev"));
+app.use("/api/country", countryRouter);
 
 export default app;
