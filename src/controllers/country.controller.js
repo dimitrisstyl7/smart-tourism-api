9f763d12fcdcde9ebb29844dc74ce20260795587
Dimitris Stylianou <dimitrisstylianou@proton.me>
2025-03-23 18:44:55 +0200

    Update country.controller.js
    - Update `findAll` function to support query parameters for filtering.
    - Simplify `create` function.
    - Create `update` function.
    - Create `delete` function.

--- src/controllers/country.controller.js
+++ src/controllers/country.controller.js
@@ -1,15 +1,28 @@
 import Country from "../models/country.model.js";
 import {validateCountryModel} from "../validations/model.validation.js";
+import {isPositiveInteger} from "../utils/number.util.js";
 
 const CountryController = {
     findAll: async (req, res) => {
+        const supportedOrderList = ["ascending", "descending", "asc", "desc"];
+        const validFields = Object.keys(Country.schema.paths);
+        const {sort, order, limit} = req.query;
+
+        // Set the filtering values with fallback values
+        const sortBy = validFields.includes(sort) ? sort : "country";
+        const sortOrder = supportedOrderList.includes(order) ? order : "asc";
+        const limitNumber = isPositiveInteger(parseInt(limit)) ? parseInt(limit) : null;
+
         try {
-            const countries = await Country.find({});
+            const query = limitNumber ?
+                Country.find({}).sort({[sortBy]: sortOrder}).limit(limitNumber) :
+                Country.find({}).sort({[sortBy]: sortOrder});
+            const countries = await query.exec();
             res.status(200).json(countries);
         } catch (error) {
             res.status(500).json({
                 errorCode: 500,
-                message: error.message || "An error occurred while retrieving countries."
+                message: "An unexpected error occurred while retrieving countries.",
             });
             console.error(error);
         }
@@ -21,30 +34,75 @@ const CountryController = {
         if (!validateCountryModel(body)) {
             return res.status(400).json({
                 errorCode: 400,
-                message: "Invalid country data. Ensure all required fields are provided and correct."
+                message: "Invalid country data. Ensure that the provided fields are correct."
             });
         }
 
         try {
             // Create and save the new country model
-            const countryModel = new Country({
-                country: body.country,
-                qualityOfLife: body.qualityOfLife,
-                adventure: body.adventure,
-                heritage: body.heritage,
-                costOfLivingIndex: body.costOfLivingIndex,
-                restaurantPriceIndex: body.restaurantPriceIndex
-            });
+            const countryModel = new Country(body);
             const createdCountry = await countryModel.save();
             res.status(201).json(createdCountry);
         } catch (error) {
-            console.error(error);
             res.status(500).json({
                 errorCode: 500,
-                message: error.message || "An error occurred while creating the country."
+                message: "An unexpected error occurred. Please try again later.",
             });
         }
     },
+
+    update: async (req, res) => {
+        // Validate provided data
+        const body = req.body;
+        if (!validateCountryModel(body, true)) {
+            return res.status(400).json({
+                errorCode: 400,
+                message: "Invalid country data. Ensure that the provided fields are correct.",
+            });
+        }
+
+        try {
+            // Check if the country exists before updating
+            const {id} = req.params;
+            const existingCountry = await Country.findById(id);
+
+            if (!existingCountry) {
+                return res.status(404).json({
+                    errorCode: 404,
+                    message: "Country not found. Please check the ID.",
+                });
+            }
+
+            const updatedCountry = await Country.findByIdAndUpdate(id, body, {returnDocument: "after"});
+            res.status(200).json(updatedCountry);
+        } catch (error) {
+            res.status(500).json({
+                errorCode: 500,
+                message: "An unexpected error occurred. Please try again later.",
+            });
+            console.error(error);
+        }
+    },
+
+    delete: async (req, res) => {
+        try {
+            // Delete the country with the given id
+            const {id} = req.params;
+            const country = await Country.findByIdAndDelete(id);
+            if (!country) {
+                res.status(404).json({
+                    errorCode: 404,
+                    message: "Country not found. Please check the ID."
+                });
+            } else res.status(200).json(country);
+        } catch (error) {
+            res.status(500).json({
+                errorCode: 500,
+                message: "An unexpected error occurred while deleting the country."
+            });
+            console.error(error);
+        }
+    }
 };
 
 export default CountryController;
