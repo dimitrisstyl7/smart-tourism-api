9d4f78e5d67bf6851d48a630da0294ae2b1807db
Dimitris Stylianou <dimitrisstylianou@proton.me>
2025-03-23 18:47:15 +0200

    Update country.router.js
    - Add `PUT /{id}` and `DELETE /{id}` http methods support.

--- src/routers/country.router.js
+++ src/routers/country.router.js
@@ -1,12 +1,18 @@
-import Router from "express";
+import {Router} from "express";
 import CountryController from "../controllers/country.controller.js";
 
-const router = new Router();
+const router = Router();
 
 // Retrieve all countries
 router.get("/", CountryController.findAll);
 
 // Create a new country
 router.post("/", CountryController.create);
+
+// Update a country by id
+router.put("/:id", CountryController.update);
+
+// Delete a country by id
+router.delete("/:id", CountryController.delete);
 
 export default router;
